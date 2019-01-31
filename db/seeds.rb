03514add90b4f773e9b25f2aa3099ce4f8446cfa
 NoraBourouis <bourouisnora@gmail.com>
2019-01-31 16:40:42 +0100

    manque les coms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+=begin
+5.times do
+  city = City.create!(city_name: Faker::GameOfThrones.city)
+end
+=end
+10.times do
+  dog = Dog.create!(name: Faker::Dog.name, city_id: rand(1..5))
+  dogsit = Dogsitter.create!(name: Faker::Name.name, city_id: rand(1..5))
+end
+
+30.times do
+  dog = Dog.find(rand(1..10))
+  dogsit = Dogsitter.find(rand(1..10))
+  if dog.city_id == dogsit.city_id
+    stroll = Stroll.create!(dog_id: dog.id, dogsitter_id: dogsit.id)
+  end
+end
